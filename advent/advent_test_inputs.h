127a27d5dd14feaa12e6a4089aeb66ff5368a9e6
arkadye <suicidalduckling@gmail.com>
2023-12-03 15:40:12 +0000

    Day 2 solutions

--- advent/advent_test_inputs.h
+++ advent/advent_test_inputs.h
@@ -13,3 +13,14 @@ constexpr const char* TEST_FILE_A = TEST_ONE_A "\n" TEST_ONE_B "\n" TEST_ONE_C "
 #define TEST_ONE_K "zoneight234"
 #define TEST_ONE_L "7pqrstsixteen"
 constexpr const char* TEST_FILE_B = TEST_ONE_F "\n" TEST_ONE_G "\n" TEST_ONE_H "\n" TEST_ONE_I "\n" TEST_ONE_J "\n" TEST_ONE_K "\n" TEST_ONE_L;
+
+constexpr const char* TEST_TWO_A = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+constexpr const char* TEST_TWO_B = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+constexpr const char* TEST_TWO_C = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+constexpr const char* TEST_TWO_D = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+constexpr const char* TEST_TWO_E = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+constexpr const char* TEST_TWO_F = R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
--- advent2/advent2.cpp
+++ advent2/advent2.cpp
@@ -21,20 +21,184 @@ namespace
 #endif
 }
 
+#include "string_line_iterator.h"
+#include "parse_utils.h"
+#include "to_value.h"
+#include "istream_line_iterator.h"
+#include "int_range.h"
+
+#include <array>
+#include <cstdint>
+#include <algorithm>
+#include <ranges>
+#include <numeric>
+
 namespace
 {
+	using BallCount = uint_fast8_t;
+	enum Color : uint8_t
+	{
+		red, green, blue, NUM
+	};
+
+	Color to_color(std::string_view in)
+	{
+		AdventCheck(!in.empty());
+		switch (in.front())
+		{
+		case 'r':
+			AdventCheck(in == "red");
+			return Color::red;
+		case 'g':
+			AdventCheck(in == "green");
+			return Color::green;
+		case 'b':
+			AdventCheck(in == "blue");
+			return Color::blue;
+		default:
+			break;
+		}
+		AdventUnreachable();
+		return Color::NUM;
+	}
+
+	class BallSet
+	{
+		std::array<BallCount, static_cast<int>(Color::NUM)> data;
+	public:
+		constexpr BallSet() : data{ 0,0,0 } {}
+		constexpr BallSet(Color a, BallCount count_a, Color b, BallCount count_b, Color c, BallCount count_c) noexcept
+		{
+			(*this)[a] = count_a;
+			(*this)[b] = count_b;
+			(*this)[c] = count_c;
+		}
+		constexpr BallCount& operator[](Color idx) noexcept { return data[static_cast<int>(idx)]; }
+		constexpr const BallCount& operator[](Color idx) const noexcept { return data[static_cast<int>(idx)]; }
+		constexpr int get_power() const
+		{
+			return std::reduce(begin(data), end(data), 1, std::multiplies<int>{});
+		}
+		void set_minima(const BallSet& other)
+		{
+			for (auto i : utils::int_range{ data.size() })
+			{
+				data[i] = std::max<BallCount>(data[i],other.data[i]);
+			}
+		}
+	};
+
+	constexpr BallSet ball_set_p1{ Color::red, 12, Color::green, 13, Color::blue, 14 };
+
+	bool check_individual_color(const BallSet& valid_set, std::string_view description)
+	{
+		description = utils::trim_left(description);
+		const auto [num_str, color_str] = utils::split_string_at_first(description, ' ');
+		const auto num_balls = utils::to_value<BallCount>(num_str);
+		const Color color = to_color(color_str);
+		return num_balls <= valid_set[color];
+	}
+
+	bool check_draw(const BallSet& valid_set, std::string_view draw_description)
+	{
+		using Range = utils::string_line_range;
+		auto check_fn = [&valid_set](std::string_view description)
+		{
+			return check_individual_color(valid_set, description);
+		};
+		return std::ranges::all_of(Range{draw_description, ','}, check_fn);
+	}
+
+	bool check_game(const BallSet& valid_set, std::string_view all_draws)
+	{
+		using Range = utils::string_line_range;
+		auto check_fn = [&valid_set](std::string_view draw_description)
+		{
+			return check_draw(valid_set, draw_description);
+		};
+
+		return std::ranges::all_of(Range{ all_draws,';' }, check_fn);
+	}
+
+	int get_game_val(const BallSet& valid_set, std::string_view line)
+	{
+		auto [game_id, draw_report] = utils::split_string_at_first(line, ':');
+		const bool is_valid = check_game(valid_set, draw_report);
+		if (!is_valid) return 0;
+		std::string_view game_id_num = utils::remove_specific_prefix(game_id, "Game ");
+		return utils::to_value<int>(game_id_num);
+	}
+
+	int solve_p1_generic(const BallSet& valid_set, std::istream& input)
+	{
+		using ILI = utils::istream_line_iterator;
+		auto trans_fn = [&valid_set](std::string_view line)
+			{
+				return get_game_val(valid_set, line);
+			};
+		return std::transform_reduce(ILI{ input }, ILI{}, 0,  std::plus<int>{}, trans_fn);
+	}
+
 	int solve_p1(std::istream& input)
 	{
-		return 0;
+		return solve_p1_generic(ball_set_p1, input);
 	}
 }
 
 namespace
 {
-	int solve_p2(std::istream& input)
+	BallSet get_from_draw(std::string_view draw)
 	{
-		return 0;
+		draw = utils::trim_left(draw);
+		auto combine_fn = [](BallSet balls, std::string_view desc)
+			{
+				desc = utils::trim_left(desc);
+				auto [num_str, color_str] = utils::split_string_at_first(desc, ' ');
+				const auto num = utils::to_value<BallCount>(num_str);
+				const Color color = to_color(color_str);
+				balls[color] = num;
+				return balls;
+			};
+
+		using SLI = utils::string_line_iterator;
+		return std::accumulate(SLI{ draw ,','}, SLI{}, BallSet{}, combine_fn);
 	}
+
+	BallSet get_from_game(std::string_view game)
+	{
+		game = utils::trim_left(game);
+		auto combine_fn = [](BallSet balls, std::string_view draw)
+			{
+				const BallSet next_draw = get_from_draw(draw);
+				balls.set_minima(next_draw);
+				return balls;
+			};
+		using SLI = utils::string_line_iterator;
+		return std::accumulate(SLI{ game , ';'}, SLI{}, BallSet{}, combine_fn);
+	}
+
+	int get_line_power(std::string_view line)
+	{
+		auto [id, game] = utils::split_string_at_first(line, ':');
+		const BallSet set = get_from_game(game);
+		return set.get_power();
+	}
+
+	int64_t solve_p2(std::istream& input)
+	{
+		using ILI = utils::istream_line_iterator;
+		return std::transform_reduce(ILI{ input }, ILI{}, int{ 0 }, std::plus<int>{}, get_line_power);
+	}
+}
+
+ResultType testcase_two_p1(std::istream& input)
+{
+	return solve_p1(input);
+}
+
+ResultType testcase_two_p2(std::istream& input)
+{
+	return solve_p2(input);
 }
 
 ResultType advent_two_p1()
