0960c2d8aaca933f84a90ecf8f377bddc018f92b
arkadye <suicidalduckling@gmail.com>
2023-12-05 18:35:14 +0000

    Add day 5 tests to test harness

--- advent/advent_test_inputs.h
+++ advent/advent_test_inputs.h
@@ -57,3 +57,38 @@ constexpr const char* TEST_FOUR_D = "Card 4 : 41 92 73 84 69 | 59 84 76 51 58  5
 constexpr const char* TEST_FOUR_E = "Card 5 : 87 83 26 28 32 | 88 30 70 12 93 22 82 36";
 constexpr const char* TEST_FOUR_F = "Card 6 : 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 static const auto TEST_FOUR_G = advent::combine_inputs(TEST_FOUR_A, TEST_FOUR_B, TEST_FOUR_C, TEST_FOUR_D, TEST_FOUR_E, TEST_FOUR_E);
+
+constexpr const char* TEST_FIVE_A =
+R"(seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4)";
