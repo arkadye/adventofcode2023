51974417cb64f96fc04421f275e2754c80209873
arkadye <suicidalduckling@gmail.com>
2023-12-10 22:07:15 +0000

    Day 9 solutions

--- advent9/advent9.cpp
+++ advent9/advent9.cpp
@@ -21,20 +21,83 @@ namespace
 #endif
 }
 
-namespace
-{
-	int solve_p1(std::istream& input)
-	{
-		return 0;
-	}
-}
+#include "istream_line_iterator.h"
+#include "string_line_iterator.h"
+#include "to_value.h"
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <numeric>
 
 namespace
 {
-	int solve_p2(std::istream& input)
+	enum class Direction : char
 	{
+		foward, backward
+	};
+
+	template <Direction dir>
+	int64_t get_next_value(const std::vector<int64_t>& vals)
+	{
+		AdventCheck(!vals.empty());
+		if (std::ranges::all_of(vals, [](int64_t v) { return v == 0; }))
+		{
+			return 0;
+		}
+
+		std::vector<int64_t> differences;
+		differences.reserve(vals.size() - 1);
+
+		std::transform(begin(vals) + 1, end(vals), begin(vals), std::back_inserter(differences), std::minus<int64_t>{});
+
+		const int64_t next_diff = get_next_value<dir>(differences);
+		if constexpr (dir == Direction::foward)
+		{
+			return vals.back() + next_diff;
+		}
+		if constexpr (dir == Direction::backward)
+		{
+			return vals.front() - next_diff;
+		}
+		AdventUnreachable();
 		return 0;
 	}
+
+	template <Direction dir>
+	int64_t get_next_value(std::string_view line)
+	{
+		std::vector<int64_t> vals;
+		std::ranges::transform(utils::string_line_range{ line , ' '}, std::back_inserter(vals), utils::to_value<int64_t>);
+		return get_next_value<dir>(vals);
+	}
+
+	template <Direction dir>
+	int64_t solve_generic(std::istream& input)
+	{
+		using ILI = utils::istream_line_iterator;
+		return std::transform_reduce(ILI{ input }, ILI{}, int64_t{ 0 }, std::plus<int64_t>{},
+			[](std::string_view sv) { return get_next_value<dir>(sv); });
+	}
+
+	int64_t solve_p1(std::istream& input)
+	{
+		return solve_generic<Direction::foward>(input);
+	}
+
+	int64_t solve_p2(std::istream& input)
+	{
+		return solve_generic<Direction::backward>(input);
+	}
+}
+
+ResultType testcase_nine_p1(std::istream& input)
+{
+	return solve_p1(input);
+}
+
+ResultType testcase_nine_p2(std::istream& input)
+{
+	return solve_p2(input);
 }
 
 ResultType advent_nine_p1()
