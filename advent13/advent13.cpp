55bf1559a5dbdfb4c4ddad8cb309741081780215
arkadye <suicidalduckling@gmail.com>
2023-12-16 01:10:05 +0000

    Day 13 solutions

--- advent13/advent13.cpp
+++ advent13/advent13.cpp
@@ -26,6 +26,10 @@ namespace
 
 namespace
 {
+	constexpr std::size_t NO_REFLECTION = std::numeric_limits<std::size_t>::max();
+	constexpr std::size_t HORIZONTAL_MUL = 100;
+	constexpr std::size_t VERTICAL_MUL = 1;
+
 	enum class Tile : char
 	{
 		ash = '.',
@@ -36,6 +40,12 @@ namespace
 	{
 		AdventCheck(c == '.' || c == '#');
 		return static_cast<Tile>(c);
+	}
+
+	std::ostream& operator<<(std::ostream& oss, Tile t)
+	{
+		oss << static_cast<char>(t);
+		return oss;
 	}
 
 	using Grid = utils::grid<Tile>;
@@ -50,65 +60,72 @@ namespace
 	{
 		const auto first = view[first_idx];
 		const auto second = view[second_idx];
+		const bool matches = (first == second);
+		//log << "\nCmp [" << first_idx << "]'" << first << "' == [" << second_idx << "]'" << second << "' = " << (matches ? "matches" : "no match");
 		return first == second;
 	}
 
-	std::optional<std::size_t> get_forward_line_of_symmetry(const auto& view)
+	bool has_reflection_at(const auto& view, std::size_t first_idx)
 	{
-		const auto left_row = view.front();
-		for(std::size_t idx : utils::int_range{std::size_t{1},view.size().reverse()})
+		std::ptrdiff_t low = first_idx - 1;
+		std::ptrdiff_t high = first_idx + 2;
+		while (low >= 0u && high < std::ssize(view))
 		{
-			if(do_lines_match(0,idx))
+			if (!do_lines_match(view, low, high))
 			{
-				auto verify_offset = [&view,idx](std::size_t offset)
-				{
-					AdventCheck(offset < idx/2);
-					const auto low_offset = offset;
-					const auto high_offset = idx - offset;
-					return do_lines_match(view,low_offset,high_offset);
-				};
-				if(std::ranges::all_of(utils::int_range{1u,idx/2},verify_offset))
-				{
-					return idx / 2;
-				}
+				return false;
+			}
+			--low;
+			++high;
+		}
+		return true;
+	}
+
+	std::size_t get_line_of_symmetry(const auto& view, std::size_t ignore_reflection)
+	{
+		if (view.empty()) return NO_REFLECTION;
+		for (auto first_idx : utils::int_range{ view.size() -1 })
+		{
+			const auto second_idx = first_idx + 1;
+			if (second_idx == ignore_reflection) continue;
+			if (do_lines_match(view, first_idx, second_idx) && has_reflection_at(view, first_idx))
+			{
+				return second_idx;
 			}
 		}
-		return std::nullopt;
+		return NO_REFLECTION;
 	}
 
-	std::optional<std::size_t> get_line_of_symmetry(const auto& view)
-	{
-		const auto forward_check = get_forward_line_of_symmetry(view);
-		if(forward_check.has_value())
-		{
-			return forward_check;
-		}
-		const auto backward_check = get_forward_line_of_symmetry(view.reverse());
-		return backward_check;
-	}
-
-	std::optional<std::size_t> get_horizontal_line_of_symmetry(const Grid& grid)
+	std::size_t get_horizontal_line_of_symmetry(const Grid& grid, std::size_t ignore_reflection)
 	{
 		const auto rows = utils::grid_helpers::row_view{grid};
-		return get_line_of_symmetry(rows);
+		return get_line_of_symmetry(rows, ignore_reflection / HORIZONTAL_MUL);
 	}
 
-	std::optional<std::size_t> get_vertical_line_of_symmetry(const Grid& grid)
+	std::size_t get_vertical_line_of_symmetry(const Grid& grid, std::size_t ignore_reflection)
 	{
 		const auto columns = utils::grid_helpers::column_view{ grid };
-		return get_line_of_symmetry(columns);
+		return get_line_of_symmetry(columns, ignore_reflection / VERTICAL_MUL);
 	}
 
-	std::size_t get_score(const Grid& grid, std::size_t horizontal_mul , std::size_t vertical_mul)
+	std::size_t get_score(const Grid& grid, std::size_t ignore_reflection)
 	{
-		const auto row_result = get_horizontal_line_of_symmetry(grid);
-		if(row_result.has_value())
+		const auto row_result = get_horizontal_line_of_symmetry(grid, ignore_reflection);
+		if(row_result != NO_REFLECTION)
 		{
-			return horizontal_mul * row_result.value();
+			return HORIZONTAL_MUL * row_result;
 		}
-		const auto column_result = get_vertical_line_of_symmetry(grid);
-		AdventCheck(column_result.has_value());
-		return vertical_mul * column_result.value();
+		const auto column_result = get_vertical_line_of_symmetry(grid, ignore_reflection);
+		if (column_result != NO_REFLECTION)
+		{
+			return VERTICAL_MUL * column_result;
+		}
+		return NO_REFLECTION;
+	}
+
+	std::size_t get_score_p1(const Grid& grid)
+	{
+		return get_score(grid, NO_REFLECTION);
 	}
 
 	std::size_t solve_p1(std::istream& input)
@@ -116,19 +133,63 @@ namespace
 		auto transform_fn = [](std::string_view block)
 		{
 			const Grid grid = parse_grid(block);
-			return get_score(grid,100,1);
+			const auto score = get_score_p1(grid);
+			log << "\nBlock:\n" << block << "\nScore=" << score << "\n";
+			AdventCheck(score != NO_REFLECTION);
+			return score;
 		};
 
-		using IBI = utils::istream_line_iterator;
+		using IBI = utils::istream_block_iterator;
 		return std::transform_reduce(IBI{input},IBI{},std::size_t{0},std::plus<std::size_t>{},transform_fn);
 	}
 }
 
 namespace
 {
-	int solve_p2(std::istream& input)
+	Tile invert(Tile t)
 	{
-		return 0;
+		switch (t)
+		{
+		case Tile::ash:
+			return Tile::rock;
+		case Tile::rock:
+			return Tile::ash;
+		}
+		AdventUnreachable();
+		return Tile::ash;
+	}
+	std::size_t get_score_p2(Grid grid)
+	{
+		const std::size_t reflection_to_ignore = get_score(grid, NO_REFLECTION);
+		for (const utils::coords& c : utils::coords_iterators::elem_range{grid.get_max_point()})
+		{
+			Tile& elem = grid.at(c);
+			elem = invert(elem);
+			const std::size_t result = get_score(grid, reflection_to_ignore);
+			if (result != NO_REFLECTION)
+			{
+				log << "\nFlipped tile at " << c;
+				return result;
+			}
+			elem = invert(elem);
+		}
+		return NO_REFLECTION;
+	}
+
+	std::size_t solve_p2(std::istream& input)
+	{
+		auto transform_fn = [](std::string_view block)
+			{
+				Grid grid = parse_grid(block);
+				log << "\nSolving grid:\n" << block;
+				const auto score = get_score_p2(std::move(grid));
+				log << "\nBlock:\n" << block << "\nScore=" << score << "\n";
+				AdventCheck(score != NO_REFLECTION);
+				return score;
+			};
+
+		using IBI = utils::istream_block_iterator;
+		return std::transform_reduce(IBI{ input }, IBI{}, std::size_t{ 0 }, std::plus<std::size_t>{}, transform_fn);
 	}
 }
 
@@ -139,7 +200,7 @@ ResultType testcase_thirteen_p1(std::istream& input)
 
 ResultType testcase_thirteen_p2(std::istream& input)
 {
-	return solve_p1(input);
+	return solve_p2(input);
 }
 
 ResultType advent_thirteen_p1()
