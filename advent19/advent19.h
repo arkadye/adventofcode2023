a1d87962983c8b88b455175d2efb51528d2fe51f
arkadye <suicidalduckling@gmail.com>
2023-12-21 17:16:25 +0000

    Day 19 partial

--- advent19/advent19.h
+++ advent19/advent19.h
@@ -2,5 +2,8 @@
 
 #include "../advent/advent_types.h"
 
+ResultType testcase_nineteen_p1(std::istream& input);
+ResultType testcase_nineteen_p2(std::istream& input);
+
 ResultType advent_nineteen_p1();
 ResultType advent_nineteen_p2();
