0d6f3389fae3b2f2c88969b6da46473f12662bba
arkadye <suicidalduckling@gmail.com>
2023-12-25 12:43:45 +0000

    Change to_value to work with a base parameter too

--- utils/to_value.h
+++ utils/to_value.h
@@ -10,17 +10,39 @@
 
 namespace utils
 {
+	template <int base = 10>
 	inline bool is_value(std::string_view sv)
 	{
+		static_assert(0 < base, "is_value only supprts positive bases.");
+		static_assert(base <= 16, "is_value only supports bases up to 16.");
 		if (sv.empty())
 		{
 			return true;
 		}
-		return (std::string_view("0123456789+-").find(sv[0]) != std::string_view::npos)
-			&& (std::all_of(begin(sv) + 1, end(sv), ::isdigit));
+
+		switch (sv.front())
+		{
+		case '+':
+		case '-':
+			sv.remove_prefix(1);
+			break;
+		default:
+			break;
+		}
+
+		
+		auto is_allowed_char = [sbase = std::size_t{base}](char c)
+			{
+				constexpr std::string_view allowed_chars{ "0123456789ABCDEF" };
+				c = std::toupper(c);
+				const auto find_result = allowed_chars.find(c);
+				return find_result < sbase;
+			};
+
+		return stdr::all_of(sv, is_allowed_char);
 	}
 
-	template <typename T>
+	template <std::integral T, int base = 10>
 	inline T to_value(std::string_view sv)
 	{
 		sv = trim_string(sv);
@@ -37,7 +59,7 @@ namespace utils
 		const char* first = sv.data();
 		const char* last = first + sv.size();
 		T value{};
-		const std::from_chars_result result = std::from_chars(first, last, value);
+		const std::from_chars_result result = std::from_chars(first, last, value, base);
 		AdventCheckMsg(result.ec == std::errc{},"ErrNo return parsing string '", sv);
 		AdventCheckMsg(result.ptr == last,"Could not convert string to value: " , sv);
 		return value;
